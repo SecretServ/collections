024181e04773900f0a16e328c1fbdd5e6d8f85f4
Askue <work@work.com>
2019-06-13 15:11:43 +0300

    3101

--- task31/task3101/Solution.java
+++ task31/task3101/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.task.task31.task3101;
 
 import java.io.File;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 /*
 Проход по дереву файлов
@@ -15,6 +14,11 @@ public class Solution {
         SortedSet<String> sortedSet = new TreeSet();
         String path = args[0];
         File resultFileAbsolutePath = new File(args[1]);
+
+        FileUtils.renameFile(resultFileAbsolutePath,
+                new File("allFilesContent.txt"));
+
+
         sol.constractor(path);
         sortedSet.addAll(sol.set);
         //sortedSet.addAll(sol.constractor(path));
@@ -29,58 +33,6 @@ public class Solution {
         File path = new File(folder);
         //Set<String> set = new HashSet<>();
         //Set<String> setFolder = new HashSet<>();
-        if (path.listFiles() == null) {
-            return;
-        } else {
-            File[] files = path.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile()) {
-                        if (file.length() <= 50) {
-                            set.add(file.getName());
-                        }
-                    } else if (file.isDirectory()) {
-                        //constractor(folder + file);
-                        setFolder.add(file.getName());
-                    }
-                }
-            } else {
-                return;
-            }
-        }
 
-
-
-        /*if (!setFolder.isEmpty()) {
-            //String
-            for (String subFolder : setFolder) {
-                //System.out.println(folder);
-                //System.out.println(subFolder);
-                String subPath = folder + "/" + subFolder;
-                //System.out.println(folder + "/" + subFolder);
-                //File
-                setFolder.remove(subFolder);
-                constractor(subPath);
-            }
-        }*/
-
-
-        if (!setFolder.isEmpty()) {
-            //for (int i = 0; i < setFolder.size(); i++) {
-                //System.out.println(folder);
-                //System.out.println(subFolder);
-                //String subPath = folder + "/" + subFolder;
-                Iterator<String> iter = setFolder.iterator();
-                while (iter.hasNext()) {
-                    String str = iter.next();
-                    System.out.println(str);
-                    //iterator.remove();
-                    setFolder.remove(str);
-                    constractor(folder + "/" + str);
-                }
-           } else {
-            return;
-        }
-        //return set;
     }
 }
